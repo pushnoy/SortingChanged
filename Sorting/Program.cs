3ca9ab6e14243816171fa56d334ad88f8b3f02cf
pushnoy <romanenko.igor2014@gmail.com>
2015-01-17 02:23:40 -0500

    Wazzzup

---
+++ Sorting/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+           
+            Console.WriteLine("Hello, this program calculates the time of array sorting by different methods: ");
+            Console.WriteLine(" - Bubble \n - Selection \n - Insertion \n - Quick sorting");
+            Console.WriteLine("Do you want to start sorting? (y / n)");
+            String userAnswer = Console.ReadLine().ToLower();
+
+            while (userAnswer == "y")
+            {
+                //Asking for the number of elements to sort 
+                int arrSize; //number of elements in array
+                Console.WriteLine("How many values do you want to sort?");
+                arrSize = Convert.ToInt32(Console.ReadLine());
+
+                //Asking for left and right array borders
+                int leftBorder, rightBorder;
+
+                Console.WriteLine("Type in the minimum value: ");
+                leftBorder = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Type in the maximum value: ");
+
+                rightBorder = leftBorder;
+                while (rightBorder <= leftBorder)
+                {
+                    rightBorder = Convert.ToInt32(Console.ReadLine());
+                    if (rightBorder <= leftBorder)
+                    {
+                        Console.WriteLine("Right border Must be bigger than left border, \n Enter the right border again!");
+                    }
+                }
+
+                //Creating an array of random integers within the selected range
+                //creating random object
+                Random rnd = new Random();
+                //creating  arrays to sort
+                int[] randomValues1 = new int[arrSize];
+                for (int i = 0; i < randomValues1.Length; i++)
+                {
+                    randomValues1[i] = rnd.Next(leftBorder, rightBorder);
+                }
+                var randomValues2 = (int[])randomValues1.Clone();
+                var randomValues3 = (int[])randomValues1.Clone();
+                var randomValues4 = (int[])randomValues1.Clone();
+
+                //calling sorting and time measuring methods
+                Sort.BubbleTimeCount(randomValues1);
+                Sort.SelectionTimeCount(randomValues2);
+                Sort.InsertionTimeCount(randomValues3);
+                Sort.QuickTimeCount(randomValues4, 0, randomValues4.Length - 1);
+
+                Console.WriteLine("Do you want to do another sorting? (y / n)");
+                userAnswer = Console.ReadLine().ToLower();
+                if(userAnswer == "y")
+                {
+                    Console.Clear();
+                }
+            }
+            Console.WriteLine("Goodbye baby");
+            Console.ReadKey();
+        }
+
+
+
+      }
+}
---
+++ Sorting/Sort.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Sorting
+{
+    class Sort
+    {
+        //Bubble sorting method
+        public static int[] BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = array.Length - 1; j > 0; j--)
+                {
+                    int temp = 0;
+                    if (array[j] < array[j - 1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+
+        //Selection sorting method
+        public static int[] SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[min])
+                    {
+                        min = j;
+                    }
+                }
+                if (min != i)
+                {
+                    int temp = array[min];
+                    array[min] = array[i];
+                    array[i] = temp;
+                }
+            }
+            return array;
+        }
+        //Insertion sorting method
+        public static int[] InsertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = i; j > 0 && array[j - 1] > array[j]; j--)
+                {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+            return array;
+        }
+        //Quick sorting method
+        public static int[] QuickSort(int[] array, int first, int last)
+        {
+            int mid, count;
+            int f = first, l = last;
+            mid = array[(f + l) / 2]; 
+            do
+            {
+                while (array[f] < mid) f++;
+                while (array[l] > mid) l--;
+                if (f <= l) 
+                {
+                    count = array[f];
+                    array[f] = array[l];
+                    array[l] = count;
+                    f++;
+                    l--;
+                }
+            } while (f < l);
+            if (first < l) QuickSort(array, first, l);
+            if (f < last) QuickSort(array, f, last);
+            return array;
+        }
+
+     
+        //bubble sorting time count
+        public static void BubbleTimeCount(int[] array)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            BubbleSort(array);
+            timer.Stop();
+            Console.WriteLine("\nBubble sorting takes: " + timer.ElapsedMilliseconds + " ms.");
+        }
+        //Selection sorting time count
+        public static void SelectionTimeCount(int[] array)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            SelectionSort(array);
+            timer.Stop();
+            Console.WriteLine("\nSelection sorting takes: " + timer.ElapsedMilliseconds + " ms.");
+        }
+        //Insertion sorting time count
+        public static void InsertionTimeCount(int[] array)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            InsertionSort(array);
+            timer.Stop();
+            Console.WriteLine("\nInsertion sorting takes: " + timer.ElapsedMilliseconds + " ms.");
+        }
+        //Quick sorting time count
+        public static void QuickTimeCount(int[] array, int first, int last)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            QuickSort(array, first, last);
+            timer.Stop();
+            Console.WriteLine("\nQuick sorting takes: " + timer.ElapsedMilliseconds + " ms.");
+        }
+    }
+}
